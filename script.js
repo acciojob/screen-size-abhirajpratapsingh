614e9a63923b9e16f4be2970bc0297175371d92a
abhiraj pratap singh <112178869+abhirajpratapsingh@users.noreply.github.com>
2025-01-29 07:54:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateWindowSize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const sizeInfo = document.getElementById('sizeInfo').querySelector('h1');
+  sizeInfo.textContent = `Width: ${width} and Height: ${height}`;
+}
+
+window.addEventListener('resize', updateWindowSize);
+updateWindowSize(); 
